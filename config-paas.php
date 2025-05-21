44d0f67b9dc632bdc9277cad15f34c749acbd37e
LizethMorales123 <134014245+LizethMorales123@users.noreply.github.com>
2025-05-20 22:37:28 -0600

    Update config-paas.php

--- config-paas.php
+++ config-paas.php
@@ -1,13 +1,16 @@
 <?php
-$server = "20.10.1.4"; // IP privada de Azure Database
-$user = "azureuser";
-$password = "AzurePassword123!";
-$database = "appdb";
-$is_paas = true;
+$server = "sqlserver-proyecto.database.windows.net"; 
+$user = "app_estudiantes_u5";
+$password = "PasswordSeguro789!"; 
+$database = "bd_u5";
+$is_paas = true; 
 
 $conn = new mysqli($server, $user, $password, $database);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+
+echo "Conexión exitosa a la base de datos!";
+
 ?>
