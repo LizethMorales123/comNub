bcf8dc5dea35e73d4305b8ab5490dfb21982c6e2
LizethMorales123 <134014245+LizethMorales123@users.noreply.github.com>
2025-05-22 22:17:43 -0600

    Update config-paas.php

--- config-paas.php
+++ config-paas.php
@@ -1,34 +1,22 @@
 <?php
-
-// Es MUY RECOMENDABLE NO HARDCODEAR CREDENCIALES
-// En un entorno de producción, usa variables de entorno para Uid y PWD.
-// Por ejemplo:
-// $serverName = getenv('DB_SERVER_NAME');
-// $databaseName = getenv('DB_DATABASE_NAME');
-// $uid = getenv('DB_USERNAME');
-// $pwd = getenv('DB_PASSWORD');
-
-$serverName = "20.10.2.4";
+$serverName = "sqlserver-proyecto.database.windows.net"; // ¡Cámbialo a esto!
 $connectionOptions = array(
     "Database" => "bd_u5",
-    "Uid" => "app_estudiantes_u5",
-    "PWD" => "PasswordSeguro789!" // ¡Recuerda, esto debe ir en variables de entorno en producción!
+    "Uid" => "adminlizeth",
+    "PWD" => "pass123$" // ¡Recuerda, esto debe ir en variables de entorno en producción!
 );
 
 try {
     // Establecer la conexión con PDO_SQLSRV
     $conn = new PDO("sqlsrv:Server=$serverName;Database=" . $connectionOptions['Database'], $connectionOptions['Uid'], $connectionOptions['PWD']);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Habilita el modo de error de excepciones para un mejor manejo de errores
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo "Conexión exitosa a la base de datos SQL Server!";
 
 } catch (PDOException $e) {
     $errorMessage = "Conexión fallida a SQL Server: " . $e->getMessage();
-
-    // Registra el error en los logs de la aplicación de Azure App Service
-    error_log($errorMessage);
-
-    // Muestra un mensaje amigable al usuario en la página web
+    // Asegúrate de que el registro de aplicación esté habilitado en Azure para ver esto
+    error_log($errorMessage); 
     die("Lo sentimos, no pudimos conectar con la base de datos. Por favor, inténtelo de nuevo más tarde o contacte al soporte.");
 }
 ?>
