5c27377fac6e715d4c4391273cbc669c53f27393
LizethMorales123 <134014245+LizethMorales123@users.noreply.github.com>
2025-05-23 01:09:28 -0600

    Update config-paas.php

--- config-paas.php
+++ config-paas.php
@@ -1,5 +1,5 @@
 <?php
-$serverName = "sqlserver-proyecto.database.windows.net"; // ¡Cámbialo a esto!
+$serverName = "20.10.2.4"; // ¡Cámbialo a esto!
 $connectionOptions = array(
     "Database" => "bd_u5",
     "Uid" => "app_estudiantes_u5",
