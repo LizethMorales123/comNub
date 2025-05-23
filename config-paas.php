86677769f0ef0ed6bf55dacc73f70165765e1fdc
LizethMorales123 <134014245+LizethMorales123@users.noreply.github.com>
2025-05-22 21:20:51 -0600

    Update config-paas.php

--- config-paas.php
+++ config-paas.php
@@ -1,5 +1,14 @@
 <?php
-$serverName = "20.10.2.4"; // Opcional: puedes especificar el puerto si no es el 1433 predeterminado
+
+// Es MUY RECOMENDABLE NO HARDCODEAR CREDENCIALES
+// En un entorno de producción, usa variables de entorno para Uid y PWD.
+// Por ejemplo:
+// $serverName = getenv('DB_SERVER_NAME');
+// $databaseName = getenv('DB_DATABASE_NAME');
+// $uid = getenv('DB_USERNAME');
+// $pwd = getenv('DB_PASSWORD');
+
+$serverName = "20.10.2.4";
 $connectionOptions = array(
     "Database" => "bd_u5",
     "Uid" => "adminlizeth",
@@ -13,8 +22,13 @@ try {
 
     echo "Conexión exitosa a la base de datos SQL Server!";
 
-
 } catch (PDOException $e) {
-    die("Conexión fallida a SQL Server: " . $e->getMessage());
+    $errorMessage = "Conexión fallida a SQL Server: " . $e->getMessage();
+
+    // Registra el error en los logs de la aplicación de Azure App Service
+    error_log($errorMessage);
+
+    // Muestra un mensaje amigable al usuario en la página web
+    die("Lo sentimos, no pudimos conectar con la base de datos. Por favor, inténtelo de nuevo más tarde o contacte al soporte.");
 }
 ?>
